ca3086d30e5ba3aa045de5534f7400d99a6a8747
Ten31-GitHub <jtwells@shaw.ca>
2024-05-06 16:24:50 -0700

    MainMenu Functionality
    This includes the functionality to map code to the buttons on the MainMenu.

---
+++ Tavern Defence/Assets/Scripts/MenuScripts/MainMenu.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        
+    }
+
+    public void StartButton() //Code after you click the Start Button
+    { 
+        Debug.Log("Button Click Successful! (Start)"); 
+    }
+
+    public void SettingsButton() //Code after you click the Settings Button
+    { 
+        Debug.Log("Button Click Successful! (Settings)"); 
+    }
+
+    public void ExitButton() //Code after you click the Exit Button
+    {
+        Debug.Log("Button Click Successful! (Exit)");
+        Application.Quit(); //Instantly Closes the Game
+    }
+}
