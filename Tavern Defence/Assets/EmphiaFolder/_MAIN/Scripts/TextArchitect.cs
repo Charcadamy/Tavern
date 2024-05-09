d4c467cb7ae91172421596b08ec3857b44446d5b
emphiaexe <169096146+emphiaexe@users.noreply.github.com>
2024-05-08 18:51:26 -0700

    coding pt 1

--- Tavern Defence/Assets/EmphiaFolder/_MAIN/Scripts/TextArchitect.cs
+++ Tavern Defence/Assets/EmphiaFolder/_MAIN/Scripts/TextArchitect.cs
@@ -1,18 +1,26 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-public class TextArchitect : MonoBehaviour
+public class TextArchitect 
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private TextMeshProUGUI tmpro_ui;
+    private TextMeshPro tmpro_world;
+    public TMP_Text tmpro => tmpro_ui != null ? tmpro_ui : tmpro_world;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string currentText => tmpro.text;
+    public string targetText { get; private set; } = "";
+    public string preText { get; private set; } = "";
+    private int preTextLength = 0;
+
+    public string fullTargetText => preText + targetText;
+
+    public enum BuildMethod {  instant, typewriter, fade }
+    public BuildMethod buildMethod = BuildMethod.typewriter;
+
+    public Color textColour { get { return tmpro.color; } set { tmpro.color = value; } }
+
+    public float speed { get { return baseSpeed * speedMultiplier; } set { speedMultiplier = value; } }
+    private const float baseSpeed = 1;
+    private float speedMultiplier = 1;
 }
