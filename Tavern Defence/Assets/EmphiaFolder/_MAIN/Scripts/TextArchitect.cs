18adea993ea0c6a0009b8bfd44908c5de271b198
emphiaexe <mars.raje.2310@gmail.com>
2024-05-09 10:42:23 -0700

    brewaking

--- Tavern Defence/Assets/EmphiaFolder/_MAIN/Scripts/TextArchitect.cs
+++ Tavern Defence/Assets/EmphiaFolder/_MAIN/Scripts/TextArchitect.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
@@ -7,11 +8,11 @@ public class TextArchitect
     private TextMeshProUGUI tmpro_ui;
     private TextMeshPro tmpro_world;
     // Creating a couple of variables, one for ui and one for world?
-    public TMP_Text tmpro => tmpro_ui != null ? tmpro_ui : tmpro_world;
+    public TMP_Text Tmpro => tmpro_ui;
     // TBH I don't know what this does. I assume it makes the variable tmpro and makes it equal to tmpro_ui and if it's null thenit becomes tmpro_world.
     // I did learn that ? is just a shortened "if" statement. : is a shortened "else" statement;
 
-    public string currentText => tmpro.text;
+    public string currentText => Tmpro.text;
     public string targetText { get; private set; } = "";
     public string preText { get; private set; } = "";
     // IG get; private set; means you can get the variable outside the class?
@@ -23,7 +24,7 @@ public class TextArchitect
     public BuildMethod buildMethod = BuildMethod.typewriter;
     // This is how the text forms in front of the player. Like word by word IG.
 
-    public Color textColor { get { return tmpro.color; } set { tmpro.color = value; } }
+    public Color textColor { get { return Tmpro.color; } set { Tmpro.color = value; } }
     // COLOURS. IT'S COLOURS WITH A U. PLEASE. I'M BUT A MERE CANADIAN. STOP SUBJECTING ME TO YOUR AMERICAN WAYS, UNITY. 
 
     public float speed { get { return baseSpeed * speedMultiplier; } set { speedMultiplier = value; } }
